187f38f002b97b1cac7e581554d0cbc2feba253b
Johan Deimert <johan.deimert@combitech.se>
2016-12-14 09:31:24 +0100

    Advent of code Day 14

---
+++ Day14/main.cpp
@@ -0,0 +1,95 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QCryptographicHash>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "Advent of code #14";
+    qDebug() << "==================" << endl;
+
+//    QString salt = "abc";
+    QString salt = "yjdafjpo";
+
+//    int partToMD5Iter = 0; // For part One
+    int partToMD5Iter = 2016;
+
+    QVector<QString> keys;
+    QVector<QString> generatedHashes;
+
+    int index = 0;
+
+    while (keys.size() < 64)
+    {
+        QString hashed;
+        if (index > generatedHashes.size()-1)
+        {
+            QString indata = salt + QString::number(index);
+            QByteArray ba(indata.toLocal8Bit());
+
+            QByteArray tmp(QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex());
+            for (int hashcount = 0 ; hashcount < partToMD5Iter ; hashcount++)
+            {
+                tmp = QCryptographicHash::hash(tmp, QCryptographicHash::Md5).toHex();
+            }
+
+            hashed = tmp;
+            generatedHashes.append(hashed);
+        }
+        else
+        {
+            hashed = generatedHashes.at(index);
+        }
+
+        int cindex = hashed.indexOf(QRegExp("([a-f0-9])\\1\\1"));
+        if (cindex > -1)
+        {
+            QString rxStr(hashed.at(cindex));
+            rxStr.append("{5}");
+            QRegExp rx(rxStr);
+
+            for (int i = 1; i < 1000 ; i++)
+            {
+                QString nextFive;
+                if ((index + i) > (generatedHashes.size()-1))
+                {
+                    QString newdata = salt + QString::number(index + i);
+                    QByteArray ba(newdata.toLocal8Bit());
+
+                    QByteArray tmp2(QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex());
+                    for (int hashcount = 0 ; hashcount < partToMD5Iter ; hashcount++)
+                    {
+                        tmp2 = QCryptographicHash::hash(tmp2, QCryptographicHash::Md5).toHex();
+                    }
+                    nextFive =tmp2;
+
+                    generatedHashes.append(nextFive);
+                }
+                else
+                {
+                    nextFive = generatedHashes.at(index + i);
+                }
+
+                if (nextFive.contains(rx))
+                {
+                    qDebug() << "Tripple Index:" << index << ", " << hashed;
+                    qDebug() << "   Five index:" << index + i << ", " << nextFive << endl;
+                    keys.append(hashed);
+                    break;
+                }
+            }
+        }
+
+        index++;
+    }
+
+    qDebug() << "=================================";
+    qDebug() << "****  Day 14, part 1 answer  ****";
+    qDebug() << "=================================" << endl;
+    qDebug() << "Index: " << index - 1;
+
+
+    exit(1);
+    return a.exec();
+}
